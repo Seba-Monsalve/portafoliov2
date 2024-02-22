f1efd348fd941f7e867ed7b9a2fa656b612386a9
Seba Monsalve <sebastian.monsalve@usach.cl>
2024-02-22 14:47:52 -0300

    Add Icon Component

--- src/components/Tecnologias.tsx
+++ src/components/Tecnologias.tsx
@@ -2,11 +2,24 @@ import { tecnologias } from "../info/tecnologias";
 import { FloatingNav } from "./FloatingNav";
 import { SkillItem } from "./SkillItem";
 import { habilidades } from '../info/habilidades';
+import { Icon } from '../utils/Icon';
+import { iconos } from "../assets/icons/iconos";
 
 export const Tecnologias = () => {
 
     return (
         <section id="tecnologias" >
+
+            {iconos.map(({fillRule, name, path_d, viewBox, }) => {
+                return <Icon 
+                    viewBox={viewBox}
+                    path_d={path_d}
+                    className="icon"
+                    fillRule={fillRule}
+                ></Icon>
+
+            })}
+
             <h2 className="seccion-titulo">Habilidades </h2>
             <p className="descripcion">Est fugiat consequat nisi ea excepteur minim ullamco consectetur ut et ex pariatur id eu. Ullamco culpa quis aliqua adipisicing pariatur do. Anim est ad voluptate consectetur voluptate ullamco sint irure do. Voluptate enim enim velit nisi excepteur sunt laboris ea Lorem tempor elit laboris irure.</p>
 
@@ -15,7 +28,7 @@ export const Tecnologias = () => {
             </ul>
 
             <br /><br />
-            
+
             <hr style={{ 'color': 'black', 'width': '100%' }} />
             <h2 className="seccion-titulo">Tecnologias </h2>
 
@@ -24,7 +37,7 @@ export const Tecnologias = () => {
                 {tecnologias.map((tecnologias) => {
                     return (
                         <li key={tecnologias.title} className="tecnologias-item">
-                            <img src={tecnologias.icon_path} alt={tecnologias.title} />
+                            <img  src={tecnologias.icon_path} alt={tecnologias.title} />
                             <hr />
                             <h3>{tecnologias.title}</h3>
                             {/* <p>{tecnologias.desc}</p> */}
---
+++ src/utils/Icon.tsx
@@ -0,0 +1,30 @@
+import { SVGProps } from "react"
+
+interface Props {
+    path_d: string[]
+    , viewBox: string
+    , style?: React.CSSProperties
+    , className?: string
+    , height?: string
+    , width?: string
+    ,fillRule?:any
+}
+
+export const Icon = ({fillRule, className, path_d, viewBox, style, height = '50px', width = '50px' }: Props) => {
+    return (
+        <svg
+            className={className}
+            style={style}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox={viewBox}
+            fill="currenColor"
+            width={width}
+            height={height}
+            fillRule={fillRule}
+        >
+            {path_d.map((path) =>
+                <path d={path} />
+            )}
+        </svg>
+    )
+}
